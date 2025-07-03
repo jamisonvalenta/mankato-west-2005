85c7e3821808c78e347b89faec53f27bedf75589
Jamison Valenta <jamison.valenta@gmail.com>
2025-07-03 09:28:41 -0600

    more financial data on admin

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,10 +14,24 @@ class AdminDashboardController extends Controller
 {
     public function index(Request $request): Response
     {
+
         return Inertia::render('admin/Dashboard', [
             'usersCount' => User::count(),
             'usersUnregisteredCount' => User::whereDoesntHave('registrations')->count(),
-            'usersAwaitingVerificationCount' => User::whereHas('registrations')->count(),
+            'usersAwaitingVerificationCount' => User::whereHas('registrations')->whereDoesntHave('verifications')->count(),
+            'confirmedAttendeesCount' => User::query()
+                ->whereHas('attendees')
+                ->whereHas('payments', function ($query) {
+                    return $query->whereNotNull('received_at');
+                })
+                ->with('attendees')
+                ->get()
+                ->reduce(function (?int $carry, $user) {
+                    return (int) $carry + $user->attendees->count();
+                }),
+
+            'paymentReceivedTotal' => Payment::whereNotNull('received_at')->sum('amount'),
+            'paymentPromisedTotal' => Payment::sum('amount'),
         ]);
     }
 }
--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -4,6 +4,9 @@
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-2 xl:grid-cols-3">
+
+
+                <!-- Users -->
                 <div class="relative aspect-video rounded-xl border border-sidebar-border/70">
                     <section class="overflow-hidden">
                         <div class="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 px-4 py-3">
@@ -15,63 +18,68 @@
                         </div>
                         <div class="divide-y divide-gray-100">
                             <section class="px-4">
-                                <dl class="divide-y divide-gray-100 text-sm leading-6">
-                                    <div class="grid grid-cols-[1fr,5em] py-4 gap-4">
-                                        <dt class="text-gray-500 text-lg leading-loose">
-                                            <UsersIcon
-                                                class="text-gray-500 w-5 h-5 inline-block mr-2"
-                                                />
-                                            Total
-                                        </dt>
-                                        <dd class="">
-                                            <Link :href="route('admin.users.index')">
-                                                <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
-                                                    {{ usersCount }}
-                                                </span>
-                                            </Link>
-                                        </dd>
+                                <dl class=" text-sm leading-6 grid grid-cols-[1fr,5em] py-4 gap-4">
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <UsersIcon
+                                            class="text-gray-500 w-5 h-5 inline-block mr-2"
+                                            />
+                                        Total
+                                    </dt>
+                                    <dd class="">
+                                        <Link :href="route('admin.users.index')">
+                                            <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
+                                                {{ usersCount }}
+                                            </span>
+                                        </Link>
+                                    </dd>
 
-                                        <dt class="text-gray-500 text-lg leading-loose">
-                                            <div class="w-5 h-5 mr-2 inline-block"></div>
-                                            Incomplete registrations
-                                        </dt>
-                                        <dd class="">
-                                            <Link :href="route('admin.users.index')">
-                                                <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
-                                                    {{ usersUnregisteredCount }}
-                                                </span>
-                                            </Link>
-                                        </dd>
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <div class="w-5 h-5 mr-2 inline-block"></div>
+                                        Incomplete registrations
+                                    </dt>
+                                    <dd class="">
+                                        <Link :href="route('admin.users.index')">
+                                            <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
+                                                {{ usersUnregisteredCount }}
+                                            </span>
+                                        </Link>
+                                    </dd>
 
-                                        <dt class="text-gray-500 text-lg leading-loose">
-                                            <ExclamationCircleIcon
-                                                v-if="usersAwaitingVerificationCount > 0"
-                                                class="text-amber-500 w-5 h-5 inline-block mr-2"
-                                                />
-                                            <div v-else class="w-5 h-5 mr-2 inline-block"></div>
-                                            Users awaiting verification
-                                        </dt>
-                                        <dd class="">
-                                            <!-- <Link :href="route('admin.verifications.index')"> -->
-                                                <span
-                                                    :class='{
-                                                        "bg-yellow-50 text-yellow-800 ring-yellow-600/20": usersAwaitingVerificationCount > 0,
-                                                        "bg-green-50 text-green-800 ring-green-600/20": usersAwaitingVerificationCount === 0,
-                                                    }'
-                                                class="inline-flex items-center rounded-md  px-3 py-2 text-xs font-medium ring-1 ring-inset ">
-                                                    {{ usersAwaitingVerificationCount }}
-                                                </span>
-                                            <!-- </Link> -->
-                                        </dd>
-                                    </div>
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <ExclamationCircleIcon
+                                            v-if="usersAwaitingVerificationCount > 0"
+                                            class="text-amber-500 w-5 h-5 inline-block mr-2"
+                                            />
+                                        <div v-else class="w-5 h-5 mr-2 inline-block"></div>
+                                        Users awaiting verification
+                                    </dt>
+                                    <dd class="">
+                                        <Link :href="route('admin.users.index')">
+                                            <span
+                                                :class='{
+                                                    "bg-yellow-50 text-yellow-800 ring-yellow-600/20": usersAwaitingVerificationCount > 0,
+                                                    "bg-green-50 text-green-800 ring-green-600/20": usersAwaitingVerificationCount === 0,
+                                                }'
+                                            class="inline-flex items-center rounded-md  px-3 py-2 text-xs font-medium ring-1 ring-inset ">
+                                                {{ usersAwaitingVerificationCount }}
+                                            </span>
+                                        </Link>
+                                    </dd>
 
-                                        <div class="flex justify-between py-4 gap-x-4">
-                                            <dt class="text-gray-500">
-
-                                            </dt>
-                                            <dd>
-                                            </dd>
-                                        </div>
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <CheckCircleIcon
+                                        class="text-green-500 w-5 h-5 inline-block mr-2"
+                                        />
+                                        # attendees (paid)
+                                    </dt>
+                                    <dd class="">
+                                        <!-- <Link :href="route('admin.verifications.index')"> -->
+                                            <span
+                                                class="bg-green-50 text-green-800 ring-green-600/20 inline-flex items-center rounded-md  px-3 py-2 text-xs font-medium ring-1 ring-inset ">
+                                                {{ confirmedAttendeesCount }}
+                                            </span>
+                                        <!-- </Link> -->
+                                    </dd>
                                 </dl>
                             </section>
                         </div>
@@ -79,8 +87,49 @@
                 </div>
 
 
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70">
+                <!-- Financials -->
+                <div class="relative aspect-video rounded-xl border border-sidebar-border/70">
+                    <section class="overflow-hidden">
+                        <div class="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 px-4 py-3">
+                            <PiggyBank class="h-10 w-10 flex-none rounded-lg bg-white object-cover border border-gray-900/10" />
+                            <div class="text-md font-medium leading-6 text-gray-900">Financials</div>
+                            <div class="relative ml-auto">
+                                <!-- <ellipsis-menu></ellipsis-menu> -->
+                            </div>
+                        </div>
+                        <div class="divide-y divide-gray-100">
+                            <section class="px-4">
+                                <dl class="text-sm leading-6 grid grid-cols-[1fr,5em] py-4 gap-4">
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <DollarSign
+                                            class="text-gray-500 w-5 h-5 inline-block mr-2"
+                                            />
+                                        Total Promised
+                                    </dt>
+                                    <dd class="">
+                                            <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
+                                                ${{ paymentPromisedTotal }}
+                                            </span>
+                                    </dd>
+
+                                    <dt class="text-gray-500 text-lg leading-loose">
+                                        <DollarSign
+                                            class="text-gray-500 w-5 h-5 inline-block mr-2"
+                                            />
+                                        Total Received
+                                    </dt>
+                                    <dd class="">
+                                            <span class="inline-flex items-center rounded-md bg-gray-50 px-3 py-2 text-xs font-medium text-gray-800 ring-1 ring-inset ring-gray-600/20">
+                                                ${{ paymentReceivedTotal }}
+                                            </span>
+                                    </dd>
+                                </dl>
+                            </section>
+                        </div>
+                    </section>
                 </div>
+
+
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70">
                 </div>
             </div>
@@ -98,12 +147,16 @@ import { Head } from '@inertiajs/vue3';
 import { Link } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 import { BellIcon, CheckCircleIcon, ExclamationCircleIcon, UsersIcon } from '@heroicons/vue/24/outline';
+import { Baby, Camera, DollarSign, PiggyBank, UsersRound, PartyPopper } from 'lucide-vue-next';
 import { CheckIcon } from '@heroicons/vue/24/solid';
 
 defineProps<{
     usersCount?: number;
     usersUnregisteredCount?: number;
     usersAwaitingVerificationCount?: number;
+    confirmedAttendeesCount?: number;
+    paymentReceivedTotal?: number;
+    paymentPromisedTotal?: number;
 }>();
 
 const breadcrumbs: BreadcrumbItem[] = [
