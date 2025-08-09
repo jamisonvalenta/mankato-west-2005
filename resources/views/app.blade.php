f2c31ea37207baed9baa4ec13704e8c92de6a17d
Jamison Valenta <jamison.valenta@gmail.com>
2025-08-09 09:24:54 -0600

    omg burn it with so much fire

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,17 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
-            html {
-                background-color: oklch(1 0 0);
-            }
-
-            html.dark {
-                background-color: oklch(0.145 0 0);
-            }
-        </style>
-
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
